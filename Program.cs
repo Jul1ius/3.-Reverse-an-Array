6d8afbc32644a61f22689594cc0ea9be44e7ccd5
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-10-23 17:53:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        int sumEven = 0;
+        int sumOdd = 0;
+ 
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                sumEven += number;
+            }
+            else
+            {
+                sumOdd += number;
+            }
+        }
+
+        int difference = sumEven - sumOdd;
+        Console.WriteLine(difference);
+    }
+}
